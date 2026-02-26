7e7dafb1809bc100b0dd795c277be0125265059c
yashas <yashasks511@gmail.com>
2026-02-26 12:31:28 +0530

    fix socket connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,11 @@
-import { io } from 'socket.io-client'; 
-const socket = io('https://realorai-9ofc.onrender.com', { autoConnect: true, reconnection: true }); 
-export default socket; 
+import { io } from 'socket.io-client';
+
+const socket = io('https://realorai-9ofc.onrender.com', {
+  autoConnect: true,
+  reconnection: true,
+  reconnectionDelay: 1000,
+  timeout: 10000,
+  transports: ['websocket', 'polling'],
+});
+
+export default socket;
